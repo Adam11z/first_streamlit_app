d112a40c8e04944ccd9e2d5f6e2137eef95b456f
Adam11z <122811799+Adam11z@users.noreply.github.com>
2023-01-16 16:39:55 +0000

    Update streamlit.app.py
    new menu option - smoothie

--- streamlit.app.py
+++ streamlit.app.py
@@ -8,4 +8,8 @@ streamlit.header('Breakfast Favourites')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')            
 streamlit.text('🐔Hard-Boiled Free-Range Egg')
-streamlit.text('🥑🍞 Avakado Toast')                             
+streamlit.text('🥑🍞 Avakado Toast')       
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+
